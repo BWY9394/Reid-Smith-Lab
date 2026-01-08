9316c94c9afb7831daea0842ab9fd3fcb337d9b6
BWY9394 <benweeyg@gmail.com>
2026-01-08 15:04:53 +1100

    Refactor fasta processing script for clarity and efficiency
    Refactor fasta processing script to streamline loading and filtering of CDS data. Added comments for clarity and improved structure.

--- fasta processing/faba_reference_qc.R
+++ fasta processing/faba_reference_qc.R
@@ -1,33 +1,35 @@
 setwd("Z:/LAB - OPV - Dugald_Reid/Genomics/faba")
-getwd()
+getwd() #confirm is same as the above. Change the directory address to whereever your raw file locations are i.e. the fasta files
 
-install.packages("seqinr")
-library('Biostrings')
+install.packages("seqinr") #same thing to install dplyr if n
 library("seqinr")
 library(dplyr)
-prot_fasta_data <-readAAStringSet("HEDIN_TMP5.check.renamed.phase.complete_proteins.fa") # Use readAAStringSet for protein sequences,  readDNAStringSet for DNA
-cds_fasta_data <- readDNAStringSet("HEDIN_TMP5.check.renamed.phase.complete_CDS.fa")
+
+#Alternative step if using Biostrings, but honestly just use seqinr if possible
+#prot_fasta_data <-readAAStringSet("HEDIN_TMP5.check.renamed.phase.complete_proteins.fa") # Use readAAStringSet for protein sequences,  readDNAStringSet for DNA
+#cds_fasta_data <- readDNAStringSet("HEDIN_TMP5.check.renamed.phase.complete_CDS.fa")
 
 
 #seqinr approach
+##protein loaded in as well just out of interest 
 prot_fasta_data <- read.fasta(file = "HEDIN_TMP5.check.renamed.phase.complete_proteins.fa", as.string = TRUE)
+##CDS is the real meat here
+###load CDS file
 cds_fasta_data <- read.fasta(file = "HEDIN_TMP5.check.renamed.phase.complete_CDS.fa", as.string = TRUE)
-str(cds_fasta_data)
+###Convert to data frame
 cds_df <- data.frame(
   id  = names(cds_fasta_data),
   seq = sapply(cds_fasta_data, paste0, collapse = ""),
   stringsAsFactors = FALSE
 )
-str(cds_df)
-View(cds_df)
-head(cds_df$id)
-
-
-write.csv(cds_df,file="HEDIN_TMP5_check_renamed_phase_complete_CDS.csv",row.names=FALSE)
+###always good to check if your fasta file has repeats i.e. is fucked from the start
 duplicated_ids <- cds_df$id[data.table::duplicated(cds_df$id)]
 
+###save if you want, otherwise skip step
+write.csv(cds_df,file="HEDIN_TMP5_check_renamed_phase_complete_CDS.csv",row.names=FALSE)
 
-
+#now ready to filter
+##Keep only longest CDS per gene
 cds_longest <- cds_df %>%
   mutate(
     gene_id = sub("\\.[0-9]+$", "", id), #create gene_id column and then replace whatever comes after \\. with a space ("")
@@ -39,19 +41,18 @@ cds_longest <- cds_df %>%
   ungroup() %>% #removes grouping metadata
   select(id, seq) %>% #drops helper columns created.
   mutate(seq = toupper(seq)) #converts seq column entries to caps
-#conver back to fasta
 
-str(cds_longest)
-
+##convert back to fasta
 seqs_split <- lapply(cds_longest$seq, function(x) strsplit(x, "")[[1]]) #splits seq column entries into separated character values for wrapping functionality when saving back into fasta
 
-#save into fasta
+##save into fasta
 write.fasta(
   sequences = seqs_split,
   names     = cds_longest$id,
   nbchar    = 70,
   file.out  = "HEDIN_TMP5.check.renamed.phase.longest_CDS.fa",
   )
-
+##sanity check
 nrow(cds_df)
 length(read.fasta("HEDIN_TMP5.check.renamed.phase.longest_CDS.fa"))
+
