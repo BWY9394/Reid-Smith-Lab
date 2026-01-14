b03260033a974ed1aaa705ea97dac90f3d6598f3
BWY9394 <benweeyg@gmail.com>
2026-01-14 11:01:35 +1100

    Update BBduk_batch.sh

--- WGS processings/BBduk_batch.sh
+++ WGS processings/BBduk_batch.sh
@@ -31,7 +31,7 @@ for R1 in ${RAW_DIR}/*_1.fq.gz; do
       out1="${OUT_DIR}/${BASE}_1_trimmed.fq.gz" \
       out2="${OUT_DIR}/${BASE}_2_trimmed.fq.gz" \
       ref=adapters.fa ktrim=r k=23 mink=11 hdist=1 \
-      qtrim=rl trimq=10 \
+      qtrim=rl trimq=10 tpe=t \
       minlen=50 \
       threads=${SLURM_CPUS_PER_TASK}
 done
