ac23f1b7eeecbe5d76c8c4e265d26e16e7611acb
BWY9394 <benweeyg@gmail.com>
2026-01-19 10:50:31 +1100

    Update Ofind.sh
    Trying to be fancy with Orthofinder settings threw an error 
    "
    OrthoFinder version 2.5.4 Copyright (C) 2014 David Emms
    
    ERROR: non-default output directory already exists: ./panfaba/Longest_AA/orthofinder_results/
    
    ERROR: An error occurred, ***please review the error messages*** they may contain useful information about the problem.
    OrthoFinder analysis complete!
    Results in: ./panfaba/Longest_AA/orthofinder_results
    "

--- Pangenomics/Ofind.sh
+++ Pangenomics/Ofind.sh
@@ -1,31 +1,19 @@
 #!/bin/bash
 #SBATCH --job-name=orthofinder_51
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=48
 #SBATCH --mem=128G
-#SBATCH -t 5-00:00 # time (D-HH:MM)
+#SBATCH -t 7-00:00 # time (D-HH:MM)
 #SBATCH --partition=week
 
 
 # Load required modules
-module purge
 module load OrthoFinder/2.5.4-foss-2020b
 
-# Set number of threads
-THREADS=32
-
 # Input directory containing protein FASTA files (51 genomes)
-INPUT_DIR="./panfaba/Longest_AA"
-
-# Output directory (OrthoFinder will create this)
-OUTPUT_DIR="./panfaba/Longest_AA/orthofinder_results" #having a folder here already created with the folder name will throw an error
+INPUT_DIR="panfaba/Longest_AA/"
 
 # Run OrthoFinder with DIAMOND (much faster for large datasets)
-orthofinder -f ${INPUT_DIR} \
-            -t ${THREADS} \
-            -a ${THREADS} \
-            -S diamond \
-            -o ${OUTPUT_DIR}
+orthofinder -f ${INPUT_DIR}
 
-echo "OrthoFinder analysis complete!"
-echo "Results in: ${OUTPUT_DIR}"
+
