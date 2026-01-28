4414edcd4c5210b255f87662264b458f58144d85
BWY9394 <benweeyg@gmail.com>
2026-01-28 12:39:28 +1100

    Update salmon_quant_transcript.sh

--- Bulk RNA-seq/Salmon/salmon_quant_transcript.sh
+++ Bulk RNA-seq/Salmon/salmon_quant_transcript.sh
@@ -3,21 +3,22 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
-#SBATCH --job-name="salC1|24"
+#SBATCH --job-name="salZBF1to28"
 #SBATCH --partition=day
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=BWeeYang@latrobe.edu.au # send-to address
+#SBATCH --mail-user=BWeeYang@ltu.edu.au # send-to address
 
 #Available salmon versions
 #Salmon/1.4.0-GCC-11.2.0
 #Salmon/1.4.0-gompi-2020b
+#/data/group/medaglab/project/BWee/Salmon/Raw/FUN_TC_Vu/
 
 #module load
 module load Salmon/1.4.0-GCC-11.2.0
 
 #Run salmon quant
 
-for fn in Raw/C{1..24}; do
+for fn in Raw/ZBF/ZBF{1..28}; do
   samp=$(basename "${fn}")
   echo "Processing sample ${samp}"
 
@@ -31,6 +32,27 @@ for fn in Raw/C{1..24}; do
   salmon quant -i salmon_indexVu -l A \
     -1 "${fn}/${samp}_1.fq.gz" \
     -2 "${fn}/${samp}_2.fq.gz" \
-    -p 8 --validateMappings -o "quantsC1_C24/${samp}_quant"
+    -p 8 --validateMappings -o "quantsZBF1_ZBF28/${samp}_quant"
 done
+
+
+# Now make an output file
+echo -e "Sample\tNumReads\tNumMapped\tPercentMapped" > quantsZBF1_ZBF28/salmon_read_summary_ZBF1_ZBF28.tsv
+
+
+
+#Extract mapping stats out into tab-separated table 
+for d in quantsZBF1_ZBF28/*_quant; do
+   samp=$(basename "$d" _quant)
+   numReads=$(jq '.num_processed' "$d/aux_info/meta_info.json")
+   numMapped=$(jq '.num_mapped' "$d/aux_info/meta_info.json")
+   pctMapped=$(jq '.percent_mapped' "$d/aux_info/meta_info.json")
+
+   echo -e "${samp}\t${numReads}\t${numMapped}\t${pctMapped}" \
+     >> quantsZBF1_ZBF28/salmon_read_summary_ZBF1_ZBF28.tsv
+done
+
+
+# Optional: email summary to yourself
+mail -s "Salmon Quant Finished" -a quantsZBF1_ZBF2/salmon_read_summary_ZBF1_ZBF28.tsv BWeeYang@ltu.edu.au <<< "All samples finished. See attached summary."
 
